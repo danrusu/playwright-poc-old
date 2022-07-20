b6cdd593a27dcdae2d5a883ada5a22c686e71b02
Dan RUSU <danginkgo@yahoo.com>
2022-07-20 19:09:48 +0300

    simple browsers loop for a test

--- src/tests/first.test.js
+++ src/tests/first.test.js
@@ -1,25 +1,27 @@
-const { chromium } = require('playwright');
+const { chromium, firefox, webkit } = require('playwright');
 
 jest.setTimeout(30000);
 
-describe('suite1', () => {
-  let browser;
-  let page;
-  let context;
+[chromium, firefox, webkit].forEach(browserDriver => {
+  describe('qatools.ro tests', () => {
+    let browser;
+    let page;
+    let context;
 
-  beforeAll(async () => {
-    browser = await chromium.launch({ headless: false, slowMo: 500 });
-    context = await browser.newContext();
-    page = await context.newPage();
-    await page.goto('http://qatools.ro');
-  });
+    beforeAll(async () => {
+      browser = await browserDriver.launch({ headless: false, slowMo: 500 });
+      context = await browser.newContext();
+      page = await context.newPage();
+      await page.goto('http://qatools.ro');
+    });
 
-  afterAll(async () => {
-    browser.close();
-  });
+    afterAll(async () => {
+      browser.close();
+    });
 
-  test('should load page', async () => {
-    expect(page).not.toBeNull();
-    expect(page.tile).not.toBeNull();
+    test(`should load page on ${browserDriver.name()}`, async () => {
+      expect(page).not.toBeNull();
+      expect(page.tile).not.toBeNull();
+    });
   });
 });
